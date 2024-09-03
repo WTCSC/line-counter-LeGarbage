b866f35b0a5f3b58993ab2b06e7c9a407ace4a45
Logan Lessen <loganlessen@gmail.com>
2024-09-03 15:36:10 -0600

    Added comments

--- line_count.py
+++ line_count.py
@@ -1,5 +1,8 @@
 def line_count():
+  #Opens file
   file = open("file.txt", "r")
+  #Splits the file into a list of individual lines, then returns the length of that list
   num_lines = len(file.readlines())
+  #Closes the file
   file.close()
   return num_lines
